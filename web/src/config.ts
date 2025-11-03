1de0b64caba885ee73a3ab7be7a08277d25e6363
Arnav Balyan <arnavb@uber.com>
2025-11-03 21:51:54 +0530

    [v0,1] Update web configs

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-import configData from '../../config.yaml'
+import configData from '../../configs/default.yaml'
 
 const config = configData as { server: { host: string; port: number } }
 export const API_BASE_URL = `http://localhost:${config.server.port}`
